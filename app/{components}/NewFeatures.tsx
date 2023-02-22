18d014c683983cb71e33579dc53893b0bb7f72a4
Noor Muhammad <connectwithnoor1@gmail.com>
2023-02-22 17:12:30 +0500

    section new-features

--- app/{components}/NewFeatures.tsx
+++ app/{components}/NewFeatures.tsx
@@ -1,6 +1,27 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import Image from 'next/image';
+import styles from '../{styles}';
+
+type Props = {
+  imgUrl: string;
+  title: string;
+  subtitle: string;
+};
+
+const NewFeatures = ({ imgUrl, subtitle, title }: Props) => (
+  <div className='flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]'>
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}
+    >
+      <div className='relative w-1/2 h-1/2'>
+        <Image src={imgUrl} alt='icon' className='object-contain' fill />
+      </div>
+    </div>
+    <h1 className='mt-[26px] font-bold text-[24px] leading-[30px] text-white'>
+      {title}
+    </h1>
+    <p className='flex-1 mt-[16px] font-normal text-[18px] text-[#b0b0b0] leading-[32px]'>
+      {subtitle}
+    </p>
   </div>
 );
 
--- app/{sections}/WhatsNew.tsx
+++ app/{sections}/WhatsNew.tsx
@@ -1,5 +1,52 @@
 'use client';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { NewFeatures, TitleText, TypingText } from '../{components}';
+import { newFeatures } from '../{constants}';
+import styles from '../{styles}';
+import { fadeIn, planetVariants } from '../{utils}/motion';
 
-const WhatsNew = () => <section>What&apos;s new section</section>;
+const WhatsNew = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      viewport={{ once: false }}
+      className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-8`}
+    >
+      {/* left */}
+
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className='flex-[0.75] flex justify-center flex-col'
+      >
+        <TypingText title="| What's new" className='' />
+        <TitleText title={<>What&apos;s new about metaversus</>} className='' />
+
+        <div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
+          {newFeatures.map((feature, index) => (
+            <NewFeatures key={feature.title + index} {...feature} />
+          ))}
+        </div>
+      </motion.div>
+
+      {/* right */}
+
+      <motion.div
+        variants={planetVariants('right')}
+        initial='hidden'
+        whileInView='show'
+        className={`${styles.flexCenter} flex-1`}
+      >
+        <div className='relative w-[90%] h-[30rem]'>
+          <Image
+            src='/whats-new.png'
+            alt='whats-new'
+            fill
+            className='object-contain'
+          />
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+);
 
 export default WhatsNew;
